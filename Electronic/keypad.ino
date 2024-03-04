29e33ec3548dd340c70513354a3f95c06731c5bc
NotSamarth <samarthvaishnav33@gmail.com>
2024-03-04 23:47:31 +0530

    keypad code added

--- Electronic/keypad.ino
+++ Electronic/keypad.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Keypad.h>
 
+const byte ROWS = 4; // Four rows
+const byte COLS = 4; // Four columns
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = {9, 8, 7, 6}; // Connect to the row pinouts of the keypad
+byte colPins[COLS] = {5, 4, 3, 2}; // Connect to the column pinouts of the keypad
+
+// Create the Keypad
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  char key = keypad.getKey(); // Get the pressed key
 
+  if (key != NO_KEY) { // Check if a key is pressed
+    Serial.println(key); // Print the pressed key
+  }
 }
